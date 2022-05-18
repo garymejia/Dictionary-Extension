3e262f73f93066347b21a63d78ee131f080370bd
tanv321 <tanvalam321@gmail.com>
2022-05-17 22:27:21 -0400

    able to show defintion for selected word

--- content.js
+++ content.js
@@ -1,10 +1,15 @@
 chrome.runtime.onMessage.addListener(function(request){
     const Http = new XMLHttpRequest();
-    const url='https://api.dictionaryapi.dev/api/v2/entries/en/hello';
+    const url='https://api.dictionaryapi.dev/api/v2/entries/en/'+request;
     Http.open("GET", url);
     Http.send();
     Http.onreadystatechange = (e) => {
-        alert(Http.responseText);
+        if(Http.readyState ==4){
+            var response = JSON.parse(Http.responseText);
+            var data  = response[0].meanings[0].definitions[0].definition
+            alert(data);
+        }
+        
     }
-    // dictionaryAPI(request);
+
 });
